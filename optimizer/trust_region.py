540bd173e678972063ce4defbc4f23be9a60bb24
Andy <72276976+Andy-math@users.noreply.github.com>
2021-06-15 14:54:45 +0800

    Update trust_region.py

--- optimizer/trust_region.py
+++ optimizer/trust_region.py
@@ -99,7 +99,7 @@ class Trust_Region_Options:
     max_iter: int
     check_rel: float = 1.0e-2
     check_abs: Optional[float] = None
-    check_iter: Optional[int] = None
+    check_iter: Optional[int] = None  # 0表示只在最优化开始前进行一次梯度检查，-1表示完全关闭检查，默认的None表示始终进行检查
     shaking: Union[Literal["x.shape[0]"], int] = "x.shape[0]"
     format: Trust_Region_Format_T
     posdef: Optional[Callable[[ndarray], str]]
