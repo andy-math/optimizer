7a27a058fe98c0155198ad98375b36664d2db171
Andy <72276976+Andy-math@users.noreply.github.com>
2021-06-28 11:30:03 +0800

    bugfix: endless loop in pcg.policies.subspace_decay

--- optimizer/_internals/pcg/policies.py
+++ optimizer/_internals/pcg/policies.py
@@ -64,6 +64,13 @@ def subspace_decay(
 
     direct = scale(g, H, _direct, delta)  # 使用精确的二次型方法确定最优缩放尺度
     lb, ub = margin(_base, constraints)  # 求出base处的约束切面上下限
+
+    # 如果约束与前进方向异号，直接返回base，啥也不做
+    if numpy.any(numpy.logical_and(_direct > ub, _direct * ub <= 0)) or numpy.any(
+        numpy.logical_and(_direct < lb, _direct * lb <= 0)
+    ):
+        return _base, exit_flag
+
     eliminated = numpy.zeros(direct.shape, dtype=numpy.bool_)  # 初始化越界表
     while True:
         # 更新越界表
