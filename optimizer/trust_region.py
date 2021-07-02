3c3d144e9798bd68765037ed695c0714feab4f8b
Andy <72276976+Andy-math@users.noreply.github.com>
2021-07-03 01:55:41 +0800

    Update trust_region.py

--- optimizer/trust_region.py
+++ optimizer/trust_region.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import annotations
 
 from typing import Callable, Final, NamedTuple, Optional, Tuple
 
@@ -30,6 +31,63 @@ class Trust_Region_Result(NamedTuple):
     delta: float
     gradient: Gradient
     success: bool
+
+
+class _FrozenState(NamedTuple):
+    f: Callable[[ndarray], float]
+    f_np: Callable[[ndarray], ndarray]
+    g: Callable[[ndarray], ndarray]
+    constraints: Tuple[ndarray, ndarray, ndarray, ndarray]
+    opts: Trust_Region_Options
+
+
+class _HessianProxy:
+    value: Hessian
+    times: int = 0
+    max_times: int
+
+    def __init__(self, value: Hessian, max_times: int) -> None:
+        self.value = value
+        self.max_times = max_times
+
+
+class _Solution:
+    state: Final[_FrozenState]
+    fval: Final[float]
+    x: Final[ndarray]
+    grad: Final[Gradient]
+    shifted_constr: Final[Tuple[ndarray, ndarray, ndarray, ndarray]]
+    hess_up_to_date: bool = False
+
+    def __init__(
+        self,
+        iter: int,
+        x: ndarray,
+        g_infnorm: Tuple[(float, float)],
+        state: _FrozenState,
+    ) -> None:
+        self.state = state
+        self.fval = state.f(x)
+        self.x = x
+        grad = make_gradient(
+            state.g,
+            x,
+            state.constraints,
+            state.opts,
+            check=GradientCheck(state.f_np, iter, *g_infnorm),
+        )
+        self.grad = grad
+        A, b, lb, ub = state.constraints
+        self.shifted_constr = (A, b - A @ x, lb - x, ub - x)
+
+    def get_hessian(self) -> _HessianProxy:
+        self.hess_up_to_date = True
+        return _HessianProxy(
+            make_hessian(self.state.g, self.x, self.state.constraints, self.state.opts),
+            self.x.shape[0]
+            if self.state.opts.shaking == "x.shape[0]"
+            else self.state.opts.shaking,
+        )
 
 
 def _input_check(
@@ -77,86 +135,67 @@ nConstraint = dyn_typing.SizeVar()
 )
 @bind_checker.bind_checker_5(input=_input_check, output=_output_check)
 def trust_region(
-    objective: Callable[[ndarray], float],
-    gradient: Callable[[ndarray], ndarray],
-    x: ndarray,
-    constraints: Tuple[ndarray, ndarray, ndarray, ndarray],
-    opts: Trust_Region_Options,
+    _objective: Callable[[ndarray], float],
+    _gradient: Callable[[ndarray], ndarray],
+    _x: ndarray,
+    _constraints: Tuple[ndarray, ndarray, ndarray, ndarray],
+    _opts: Trust_Region_Options,
 ) -> Trust_Region_Result:
-    class HProxy:
-        shaking: Final[int] = (
-            x.shape[0] if opts.shaking == "x.shape[0]" else opts.shaking
-        )
-        H: Hessian
-        up_to_date: bool = True
-        times: int = 0
-
-        def __init__(self, x: ndarray) -> None:
-            self.H = make_hessian(gradient, x, constraints, opts)
-
     def objective_ndarray(x: ndarray) -> ndarray:
-        return numpy.array([objective(x)])
+        return numpy.array([_objective(x)])
 
-    def get_info(
-        x: ndarray, check: GradientCheck
-    ) -> Tuple[Gradient, Tuple[ndarray, ndarray, ndarray, ndarray]]:
-        new_grad = make_gradient(gradient, x, constraints, opts, check=check)
-        A, b, lb, ub = constraints
-        _constr_shifted = (A, b - A @ x, lb - x, ub - x)
-        return new_grad, _constr_shifted
-
-    assert linneq.check(x.reshape(-1, 1), constraints)
-
-    fval = objective(x)
-    hessian = HProxy(x)
-    grad, _constr_shifted = get_info(
-        x, GradientCheck(objective_ndarray, 0, numpy.inf, 0.0)
-    )
-
-    iter = 0
-    delta = opts.init_delta
-    init_grad_infnorm: Final[float] = grad.infnorm
-    old_fval, stall_iter = fval, 0
-
-    options.output(iter, fval, grad.infnorm, None, hessian.H.ill, opts, 1)
+    assert linneq.check(_x.reshape(-1, 1), _constraints)
 
     hessian_force_shake: Optional[bool] = False
 
+    state = _FrozenState(_objective, objective_ndarray, _gradient, _constraints, _opts)
+    sol0 = _Solution(0, _x, (numpy.inf, 0.0), state)
+
+    iter, delta = 0, state.opts.init_delta
+    old_fval, stall_iter = sol0.fval, 0
+
+    hessian = sol0.get_hessian()
+
+    options.output(
+        iter, sol0.fval, sol0.grad.infnorm, None, hessian.value.ill, state.opts, 1
+    )
+
+    sol = sol0
     while True:
         # 失败情形的截止条件放在最前是因为pcg失败时的continue会导致后面代码被跳过
-        if delta < opts.tol_step:  # 信赖域太小
-            return Trust_Region_Result(x, iter, delta, grad, success=False)
-        if iter > opts.max_iter:  # 迭代次数超过要求
-            return Trust_Region_Result(x, iter, delta, grad, success=False)
+        if delta < state.opts.tol_step:  # 信赖域太小
+            return Trust_Region_Result(sol.x, iter, delta, sol.grad, success=False)
+        if iter > state.opts.max_iter:  # 迭代次数超过要求
+            return Trust_Region_Result(sol.x, iter, delta, sol.grad, success=False)
 
         # hessian过期则重新采样
         assert hessian_force_shake is not None
-        if hessian.times > hessian.shaking and not hessian.up_to_date:
-            hessian = HProxy(x)
+        if hessian.times > hessian.max_times and not sol.hess_up_to_date:
+            hessian = sol.get_hessian()
         elif hessian_force_shake:
-            hessian = HProxy(x)
+            hessian = sol.get_hessian()
         hessian_force_shake = None
 
         # PCG
-        pcg_status = pcg.pcg(grad.value, hessian.H, _constr_shifted, delta)
+        pcg_status = pcg.pcg(sol.grad.value, hessian.value, sol.shifted_constr, delta)
         iter += 1
         hessian.times += 1
 
         # PCG失败recover
         if pcg_status.x is None:
             assert hessian_force_shake is None
-            if not hessian.up_to_date:
+            if not sol.hess_up_to_date:
                 hessian_force_shake = True
             else:
                 hessian_force_shake = False
                 delta /= 4.0
             options.output(
                 iter,
-                fval,
-                grad.infnorm,
+                sol.fval,
+                sol.grad.infnorm,
                 pcg_status,
-                hessian.H.ill,
-                opts,
+                hessian.value.ill,
+                state.opts,
                 hessian.times,
             )
             continue
@@ -164,64 +203,71 @@ def trust_region(
         assert pcg_status.size is not None
 
         # 更新步长、试探点、试探函数值
-        new_x: ndarray = x + pcg_status.x
-        new_fval: float = objective(new_x)
+        new_sol = _Solution(
+            iter, sol.x + pcg_status.x, (sol.grad.infnorm, sol0.grad.infnorm), state
+        )
 
         # 根据下降率确定信赖域缩放
-        reduce: float = new_fval - fval
+        reduce: float = new_sol.fval - sol.fval
         ratio = (
             0 if reduce * pcg_status.fval <= 0 else min(reduce / pcg_status.fval, 1.0)
         )
         if ratio >= 0.75 and pcg_status.size >= 0.9 * delta and reduce < 0:
             delta *= 2
         elif ratio <= 0.25 or reduce > 0:
-            if not hessian.up_to_date:
+            if not sol.hess_up_to_date:
                 assert hessian_force_shake is None
                 hessian_force_shake = True
             else:
                 delta = pcg_status.size / 4.0
 
         # 对符合下降要求的候选点进行更新
-        hessian_out_of_date: bool = False
-        if new_fval < fval:
-            x, fval, hessian_out_of_date = new_x, new_fval, True
-            grad, _constr_shifted = get_info(
-                x,
-                GradientCheck(objective_ndarray, iter, grad.infnorm, init_grad_infnorm),
-            )
+        old_sol: _Solution = sol
+        if new_sol.fval < sol.fval:
+            sol = new_sol
             # 下降量超过设定则重置延迟计数
-            if opts.abstol_fval is not None and old_fval - fval < opts.abstol_fval:
+            if (
+                state.opts.abstol_fval is not None
+                and old_fval - sol.fval < state.opts.abstol_fval
+            ):
                 stall_iter += 1
             else:
-                old_fval, stall_iter = fval, 0
+                old_fval, stall_iter = sol.fval, 0
 
         options.output(
-            iter, fval, grad.infnorm, pcg_status, hessian.H.ill, opts, hessian.times
+            iter,
+            sol.fval,
+            sol.grad.infnorm,
+            pcg_status,
+            hessian.value.ill,
+            state.opts,
+            hessian.times,
         )
 
         # PCG正定收敛
         if pcg_status.flag in (pcg.Flag.RESIDUAL_CONVERGENCE, pcg.Flag.POLICY_ONLY):
             # 梯度足够小的case无关乎hessian信息
-            if grad.infnorm < opts.tol_grad:
-                return Trust_Region_Result(x, iter, delta, grad, success=True)
+            if sol.grad.infnorm < state.opts.tol_grad:
+                return Trust_Region_Result(sol.x, iter, delta, sol.grad, success=True)
             # 步长足够小的case要考虑hessian更新
-            if pcg_status.size < opts.tol_step:
-                if not hessian.up_to_date:
+            if pcg_status.size < state.opts.tol_step:
+                if not old_sol.hess_up_to_date:
                     assert hessian_force_shake or hessian_force_shake is None
                     hessian_force_shake = True
                     continue
-                return Trust_Region_Result(x, iter, delta, grad, success=True)
+                return Trust_Region_Result(sol.x, iter, delta, sol.grad, success=True)
 
         # 下降量过低的case要考虑hessian更新
-        if opts.max_stall_iter is not None and stall_iter >= opts.max_stall_iter:
-            if not hessian.up_to_date:
+        if (
+            state.opts.max_stall_iter is not None
+            and stall_iter >= state.opts.max_stall_iter
+        ):
+            if not old_sol.hess_up_to_date:
                 assert hessian_force_shake or hessian_force_shake is None
                 hessian_force_shake = True
                 continue
-            return Trust_Region_Result(x, iter, delta, grad, success=True)
+            return Trust_Region_Result(sol.x, iter, delta, sol.grad, success=True)
 
-        if hessian_out_of_date:
-            hessian.up_to_date = False
         if hessian_force_shake is not None:
             assert hessian_force_shake
             continue
