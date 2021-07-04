23048634793873d287cfb56b62aaa52b6907f4c6
Andy <72276976+Andy-math@users.noreply.github.com>
2021-07-04 08:06:03 +0800

    Remove unused code

--- optimizer/_internals/trust_region/grad_maker.py
+++ optimizer/_internals/trust_region/grad_maker.py
@@ -2,8 +2,6 @@ from typing import Callable, NamedTuple, Optional, Tuple
 
 import numpy
 from numpy import ndarray
-from optimizer._internals.common.findiff import findiff
-from optimizer._internals.common.hessian import Hessian
 from optimizer._internals.trust_region.grad_check import gradient_check
 from optimizer._internals.trust_region.grad_cutoff import gradient_cutoff
 from optimizer._internals.trust_region.options import Trust_Region_Options
