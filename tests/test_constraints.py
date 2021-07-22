e408fd1d97195ad4a6b6d1da7016920a2c902e00
Andy <72276976+Andy-math@users.noreply.github.com>
2021-07-23 03:11:28 +0800

    Update test_constraints.py

--- tests/test_constraints.py
+++ tests/test_constraints.py
@@ -15,22 +15,20 @@ f = x^2 + (y-2)^2 = x^2 + y^2 - 4y + 4
 """
 
 
-def func(_x: ndarray) -> float:
-    x: float
-    y: float
-    x, y = _x
-    return x * x + (y - 2) * (y - 2)
-
-
-def grad(_x: ndarray) -> ndarray:
-    x: float
-    y: float
-    x, y = _x
-    return numpy.array([2 * x, 2 * y - 4])
-
-
 class Test_constraints:
     def test1(self) -> None:
+        def func(_x: ndarray) -> float:
+            x: float
+            y: float
+            x, y = _x
+            return x * x + (y - 2) * (y - 2)
+
+        def grad(_x: ndarray) -> ndarray:
+            x: float
+            y: float
+            x, y = _x
+            return numpy.array([2 * x, 2 * y - 4])
+
         constr_A = numpy.array(
             [
                 [-1, -1],
@@ -56,8 +54,41 @@ class Test_constraints:
         )
         assert result.success
         assert numpy.all(result.x.round(6) == numpy.array([0.0, 1.0]))
-        # assert 3 < result.iter < 20
+        assert 3 < result.iter < 20
+
+    def test2(self) -> None:
+        def func(_x: ndarray) -> float:
+            x: float
+            y: float
+            x, y = _x
+            return (x + 2) * (x + 2) + (y - 2) * (y - 2)
+
+        def grad(_x: ndarray) -> ndarray:
+            x: float
+            y: float
+            x, y = _x
+            return numpy.array([2 * (x + 2), 2 * (y - 2)])
+
+        constr_A = numpy.empty((0, 2))
+        constr_b = numpy.ones((0,))
+        constr_lb = numpy.array([-1, -1], dtype=numpy.float64)
+        constr_ub = numpy.array([1, 1], dtype=numpy.float64)
+
+        opts = trust_region.Trust_Region_Options(max_iter=500)
+        opts.check_rel = 1
+        opts.check_abs = 1e-6
+        result = trust_region.trust_region(
+            func,
+            grad,
+            numpy.array([0.9, 0.9]),
+            (constr_A, constr_b, constr_lb, constr_ub),
+            opts,
+        )
+        assert result.success
+        assert numpy.all(result.x.round(6) == numpy.array([-1.0, 1.0]))
+        assert 3 < result.iter < 20
 
 
 if __name__ == "__main__":
     Test_constraints().test1()
+    Test_constraints().test2()
