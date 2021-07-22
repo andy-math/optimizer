1e47b54c7373d474c47ff2d9121a66a3cce9a5d3
Andy <72276976+Andy-math@users.noreply.github.com>
2021-07-23 00:36:58 +0800

    Update test_pcg.py

--- tests/test_pcg.py
+++ tests/test_pcg.py
@@ -41,6 +41,41 @@ class Test_pcg:
             g = H @ status.x + g
             assert numpy.abs(g).max() < math.sqrt(EPS)
 
+    def test_nonconvex(self) -> None:
+        dim = 10
+        delta = 99999999
+        constraints = (
+            numpy.empty((0, dim)),
+            numpy.empty((0,)),
+            numpy.full((dim,), -numpy.inf),
+            numpy.full((dim,), numpy.inf),
+        )
+        for _ in range(1000):
+            H = numpy.random.randn(dim, dim)
+            H = (H.T + H) / 2  # type: ignore
+            V: ndarray
+            E: ndarray
+            E, V = numpy.linalg.eig(H)  # type: ignore
+            E = E.real
+            assert numpy.abs(
+                V @ numpy.diag(E) @ V.T - H  # type: ignore
+            ).max() < math.sqrt(EPS)
+
+            g = numpy.random.randn(dim)
+            status, dir = pcg._implimentation(
+                g, H, numpy.sqrt(numpy.sum(H * H, axis=1)), constraints, delta
+            )
+            if status.flag == pcg.Flag.RESIDUAL_CONVERGENCE:
+                assert dir is None
+                g = H @ status.x + g
+                assert numpy.abs(g).max() < math.sqrt(EPS)
+            else:
+                assert status.flag == pcg.Flag.NEGATIVE_CURVATURE
+                assert status.iter < dim
+                assert dir is not None
+                assert dir @ H @ dir < math.sqrt(EPS)
+
 
 if __name__ == "__main__":
     Test_pcg().test_convex()
+    Test_pcg().test_nonconvex()
