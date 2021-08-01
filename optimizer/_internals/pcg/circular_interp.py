c427300d57016caf7d463f01a34cb51107f58349
Andy <72276976+Andy-math@users.noreply.github.com>
2021-08-02 07:53:32 +0800

    Update circular_interp.py

--- optimizer/_internals/pcg/circular_interp.py
+++ optimizer/_internals/pcg/circular_interp.py
@@ -4,6 +4,7 @@
 from typing import Tuple
 
 import numpy
+
 from optimizer._internals.common.norm import norm_l2, safe_normalize
 from overloads import bind_checker
 from overloads.shortcuts import assertNoInfNaN
@@ -37,17 +38,16 @@ def circular_interp(direct1: ndarray, direct2: ndarray) -> ndarray:
     d2_l2norm = norm_l2(direct2)
 
     # 两个都没有时，返回任意一个都是正确的零向量
-    if not d2_l2norm:
-        return direct1.reshape(-1, 1)
-    if not d1_l2norm:
-        return direct2.reshape(-1, 1)
+    if not d2_l2norm or not d1_l2norm:
+        return numpy.stack((direct1, direct2), axis=1)
 
     cos: float = float(direct1 @ direct2) / (d1_l2norm * d2_l2norm)
     if numpy.abs(cos - 1) < numpy.sqrt(_eps):
-        return direct1.reshape(-1, 1)  # 相似度太高，返回任意一个都正确
+        return numpy.stack((direct1, direct2), axis=1)
     else:
         rad = numpy.linspace(0, numpy.arccos(cos), num=num)
         w1, w2 = numpy.cos(rad), numpy.sin(rad)
-        direct2 = (direct2 - direct1 * w1[-1]) / w2[-1]  # 正交化
-        x: ndarray = w1 * direct1.reshape(-1, 1) + w2 * direct2.reshape(-1, 1)
-        return x
+        direct1, direct2 = direct1.reshape(-1, 1), direct2.reshape(-1, 1)
+        orthogonal = (direct2 - direct1 * w1[-1]) / w2[-1]  # 正交化
+        x: ndarray = w1 * direct1 + w2 * orthogonal
+        return numpy.concatenate((direct1, x, direct2), axis=1)  # type: ignore
